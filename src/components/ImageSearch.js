3d3bb64c9e6ff360addfd8581449a2773b66254e
Carol Lee <CLee@thrillworks.com>
2020-10-06 22:31:13 -0400

    Integrated tailwind and Pixabay API

---
+++ src/components/ImageSearch.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const ImageSearch = ({ searchText }) => {
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    searchText(text);
+  }
+
+  return (
+    <div className="max-w-sm rounded overlfow-hidden my-10 mx-auto">
+      <form 
+        className="w-full max-w-sm"
+        onSubmit={onSubmit}
+      >
+        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+          <input 
+            className="appearance-none bg-transparent border-none w-full text-gray-700 
+                      mr-3 py-1 px-2 leading-tight focuts:outline-none"
+            type="text"
+            placeholder="Search Image Term..." 
+            onChange={e => setText(e.target.value)}
+          />
+          <button 
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 
+                      hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      
+    </div>
+  )
+}
+
+export default ImageSearch;
